1d730be1f774d0e77dca0e0a6413b428f1f0ce75
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2021-12-02 14:08:16 -0300

    Add down vote unit tests

--- tests/votes.test.js
+++ tests/votes.test.js
@@ -5,6 +5,18 @@ const sut = recommendationsService
 
 
 describe('Test castUpVote', () => {
+	test('Should return null for inexistent recommendation', async () => {
+		const id = 10
+		const recommendation = undefined
+
+		jest.spyOn(recommendationsRepository, 'findRecommendationById')
+			.mockImplementationOnce(() => recommendation)
+
+		const result = await sut.castUpVote({id})
+
+		expect(result).toBeNull()
+	})
+
 	test('Should incresse the score for existent recommendation', async () => {
 		const id = 10
 		const recommendation = {
@@ -27,6 +39,10 @@ describe('Test castUpVote', () => {
 
 		expect(result).toEqual(upVoted)
 	})
+})
+
+describe('Test castDownVote', () => {
+	const limit = -4
 
 	test('Should return null for inexistent recommendation', async () => {
 		const id = 10
@@ -35,8 +51,70 @@ describe('Test castUpVote', () => {
 		jest.spyOn(recommendationsRepository, 'findRecommendationById')
 			.mockImplementationOnce(() => recommendation)
 
-		const result = await sut.castUpVote({id})
+		const result = await sut.castDownVote({id})
 
 		expect(result).toBeNull()
 	})
+
+	test('Should decrees the score for existent recommendation', async () => {
+		const id = 10
+		const recommendation = {
+			id,
+			name: 'Youtube name',
+			youtubeLink: 'https://www.youtube.com/playlist?list=PLpSJMw6H4PFMOJHMULTxKNOEw7g1cBuyP',
+			score: limit + 1,
+		}
+		const downVoted = {
+			...recommendation,
+			score: recommendation.score - 1
+		}
+
+		jest.spyOn(recommendationsRepository, 'findRecommendationById')
+			.mockImplementationOnce(() => recommendation)
+		jest.spyOn(recommendationsRepository, 'changeScore')
+			.mockImplementationOnce(() =>  downVoted)
+
+		const result = await sut.castDownVote({id})
+
+		expect(result).toEqual(downVoted)
+	})
+
+	test('Should delete recommendation for limit score -5', async () => {
+		const id = 10
+		const recommendation = {
+			id,
+			name: 'Youtube name',
+			youtubeLink: 'https://www.youtube.com/playlist?list=PLpSJMw6H4PFMOJHMULTxKNOEw7g1cBuyP',
+			score: limit,
+		}
+
+		jest.spyOn(recommendationsRepository, 'findRecommendationById')
+			.mockImplementationOnce(() => recommendation)
+		jest.spyOn(recommendationsRepository, 'deleteRecommendationById')
+			.mockImplementationOnce(() => undefined)
+
+		const result = await sut.castDownVote({id})
+
+		expect(result).toEqual('deleted')
+	})
+
+	test('Should delete recommendation for limit score below -5', async () => {
+		const id = 10
+		const recommendation = {
+			id,
+			name: 'Youtube name',
+			youtubeLink: 'https://www.youtube.com/playlist?list=PLpSJMw6H4PFMOJHMULTxKNOEw7g1cBuyP',
+			score: limit - 1,
+		}
+
+		jest.spyOn(recommendationsRepository, 'findRecommendationById')
+			.mockImplementationOnce(() => recommendation)
+		jest.spyOn(recommendationsRepository, 'deleteRecommendationById')
+			.mockImplementationOnce(() => undefined)
+
+		const result = await sut.castDownVote({id})
+
+		expect(result).toEqual('deleted')
+	})
+
 })
