3ea1b2bb20dd306c50fbb991175794585ef79ad4
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2021-12-02 13:37:30 -0300

    Down vote post end point implemented

--- src/services/recommendationsService.js
+++ src/services/recommendationsService.js
@@ -16,7 +16,29 @@ const castUpVote = async ({ id }) => {
 	return upVoted
 }
 
+const castDownVote = async ({ id }) => {
+	const recommendation = await recommendationsRepository
+		.findRecommendationById({ id })
+		
+	if (!recommendation) return null
+	const { score } = recommendation
+
+	if (score <= -4) {
+		await recommendationsRepository.deleteRecommendationById({ id })
+		return 'deleted'
+	}
+
+	const body = {
+		id,
+		newScore: score - 1,
+	}
+	const downVoted = await recommendationsRepository.changeScore(body)
+
+	return downVoted
+}
+
 
 export {
 	castUpVote,
+	castDownVote,
 }
